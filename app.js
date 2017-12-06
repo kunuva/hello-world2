4a8bd0419c7e72c236f33eb936198863cfe8686e
kunuva <jaydenmayger@gmail.com>
2017-12-06 18:30:48 +1000

    Saving

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-var firstName = "Jayden";
-var lastName = "Mayger";
-return firstName + " " + lastName;
-
+var arr = [{
+	firstName: "Jayden", 
+	lastName: "Mayger", 
+	Address: [
+		Street: "29 Bernard Cres", 
+		Town: "Kingaroy", 
+		Phone: 0408 925 683]}];
+console.log(arr.firstName);
